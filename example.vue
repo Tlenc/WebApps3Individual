8bf9db5cf279459bb088a5b904a0550938cf5ce1
Tlenc <tlenciauskas@gmail.com>
2022-03-18 07:33:26 +0000

    webAppCli

---
+++ example.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>{{msg}}</div>
+</template>
+
+<script>
+export default {
+    name: 'hello',
+    data(){
+        return{
+            msg: "Welcome to vue example"
+        }
+    }
+}
+</script>
