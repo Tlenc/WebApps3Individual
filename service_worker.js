9829493a7221a97c2d8b8e250078767630bccce1
Tlenc <tlenciauskas@gmail.com>
2022-03-18 06:14:24 +0000

    service worker

---
+++ service_worker.js
@@ -0,0 +1,26 @@
+var cacheName = 'lesson_store';
+var cacheFiles = [
+    'index.html',
+    'service_worker.js',
+    'lessons.webmanifest'
+];
+
+self.addEventListener('install',(e)=>{
+    console.log('[service worker] install.');
+    e.waitUntil(
+        caches.open(cacheName).then((cache)=>{
+            console.log('[service worker] caching files.');
+            return cache.addAll(cacheFiles);
+        })
+    )
+   
+}
+);
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (r){
+            return r
+        })
+    )
+})
