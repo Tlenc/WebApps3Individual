67f95b97b9eb401098583edd36efb86b27c26838
Tlenc <tlenciauskas@gmail.com>
2022-03-18 06:26:16 +0000

    Update service_worker.js

--- service_worker.js
+++ service_worker.js
@@ -20,8 +20,13 @@ self.addEventListener('install',(e)=>{
 self.addEventListener('fetch', function (e) {
     e.respondWith(
         caches.match(e.request).then(function (r){
-            console.log('[Service worker] Fetching resource: ' + e.request.url);
-            return r
+            return r || fetch(e.request).then(function (response){
+                return caches.open(cacheName).then(function (cache){
+                    cache.put(e.request,response.clone());
+                    return response;
+                });
+            });
+            
         })
-    )
-})
+    );
+});
